9c7d5d2c021e56689950a1bd05dc89cd8ce61231
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-11-07 06:05:59 +0100

    Add title and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<ChakraProvider>
+			<Head>
+				<title>AquaHome</title>
+			</Head>
 			<Component {...pageProps} />
 		</ChakraProvider>
 	);
